d8f32441205f8bf232065734fba19121d8fbfe91
NathanBhanji <nathan.bhanji@letsdothis.com>
2026-02-19 12:00:33 +0000

    Update tailchat to v0.1.3

--- Formula/tailchat.rb
+++ Formula/tailchat.rb
@@ -1,30 +1,30 @@
 class Tailchat < Formula
   desc "End-to-end encrypted terminal chat over Tailscale"
   homepage "https://github.com/NathanBhanji/tail-chat"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.2/tailchat_darwin_amd64.tar.gz"
-      sha256 "18f7778ce66a97da039d70b687c070c6fb4834234daea575509e792f3f29f881"
+      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.3/tailchat_darwin_amd64.tar.gz"
+      sha256 "da7046e25cb8162baedc4d0a23395344986fe36bb7394b70b37d24d95484864b"
     end
 
     on_arm do
-      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.2/tailchat_darwin_arm64.tar.gz"
-      sha256 "0e26550f6233682fe087cf5a0bc7b9faee95359b871069dabd7cf6e03832c493"
+      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.3/tailchat_darwin_arm64.tar.gz"
+      sha256 "517a755993c1b0f84c9ba1829729fa12e1bd802b429f23940322362d9aeb74b9"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.2/tailchat_linux_amd64.tar.gz"
-      sha256 "054f1cce5301b56423c05d4f68d5820035c5d959260b3ba87481ab244b716798"
+      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.3/tailchat_linux_amd64.tar.gz"
+      sha256 "754a851134625d5b1afa41b2b0b0b5a06492f74ab6d100af5da137859a84fd0b"
     end
 
     on_arm do
-      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.2/tailchat_linux_arm64.tar.gz"
-      sha256 "c8193c2ab8cceed5cc1f8ea04fa32cb6d6d244c5c7b258ec67a271f9c79e66b0"
+      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.3/tailchat_linux_arm64.tar.gz"
+      sha256 "f395a94397f71baf63adb79f254f41a0caf9670fcda3450bc19cab9c3095e708"
     end
   end
 
