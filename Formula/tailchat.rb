500740966631111be5866a3b2f38a3b32631338a
NathanBhanji <nathan.bhanji@letsdothis.com>
2026-02-19 11:47:17 +0000

    Update tailchat to v0.1.2

--- Formula/tailchat.rb
+++ Formula/tailchat.rb
@@ -1,30 +1,30 @@
 class Tailchat < Formula
   desc "End-to-end encrypted terminal chat over Tailscale"
   homepage "https://github.com/NathanBhanji/tail-chat"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.1/tailchat_darwin_amd64.tar.gz"
-      sha256 "ca690bf3c1036c1d9b9998e46db39bcf9fb303d08c7966488f0e3bc431bc3c44"
+      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.2/tailchat_darwin_amd64.tar.gz"
+      sha256 "18f7778ce66a97da039d70b687c070c6fb4834234daea575509e792f3f29f881"
     end
 
     on_arm do
-      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.1/tailchat_darwin_arm64.tar.gz"
-      sha256 "26b4e6741a3de37f38f36c67e9919e9d6e5cdc8c616523d82e3661f8b6cd7360"
+      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.2/tailchat_darwin_arm64.tar.gz"
+      sha256 "0e26550f6233682fe087cf5a0bc7b9faee95359b871069dabd7cf6e03832c493"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.1/tailchat_linux_amd64.tar.gz"
-      sha256 "c9b975d47327b6dd7cd762d4dda8c70e745b6f7182f858e0e225373c5c932f09"
+      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.2/tailchat_linux_amd64.tar.gz"
+      sha256 "054f1cce5301b56423c05d4f68d5820035c5d959260b3ba87481ab244b716798"
     end
 
     on_arm do
-      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.1/tailchat_linux_arm64.tar.gz"
-      sha256 "962ee94fa731cb6801f131cf4d4f35dd23fd6fa65136b5e6e87923d1ed083bab"
+      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.2/tailchat_linux_arm64.tar.gz"
+      sha256 "c8193c2ab8cceed5cc1f8ea04fa32cb6d6d244c5c7b258ec67a271f9c79e66b0"
     end
   end
 
