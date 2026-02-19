5c34ccf03220e7adab03a759857f98a636666df2
NathanBhanji <nathan.bhanji@letsdothis.com>
2026-02-19 13:40:35 +0000

    Update tailchat to v0.2.0

--- Formula/tailchat.rb
+++ Formula/tailchat.rb
@@ -1,30 +1,30 @@
 class Tailchat < Formula
   desc "End-to-end encrypted terminal chat over Tailscale"
   homepage "https://github.com/NathanBhanji/tail-chat"
-  version "0.1.6"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.6/tailchat_darwin_amd64.tar.gz"
-      sha256 "e56f42007de33e032f979fecce9714e3f23c2c7b8209ca83a844fb9a15eece54"
+      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.2.0/tailchat_darwin_amd64.tar.gz"
+      sha256 "0612b61e71af86fdcdf06268818ed6a3ae608e5881e3233c440ae7cab8600984"
     end
 
     on_arm do
-      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.6/tailchat_darwin_arm64.tar.gz"
-      sha256 "07f0406c44ebb2f323467932f415a2571de3e3888c027a23fc3a988ba067d85d"
+      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.2.0/tailchat_darwin_arm64.tar.gz"
+      sha256 "43876f5c44dbcc3b58978f8393a7bf39bf9d2794c7cc6109067ff3b4a2d2bfa7"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.6/tailchat_linux_amd64.tar.gz"
-      sha256 "ddb71004b06200a52f1ebcf70f169c236ef076e863eda6fed7a4540ca39c8e66"
+      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.2.0/tailchat_linux_amd64.tar.gz"
+      sha256 "122d546f97802456f08df5bed5b4891ca751d31aa7d8664d2627fda37b9b9c47"
     end
 
     on_arm do
-      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.6/tailchat_linux_arm64.tar.gz"
-      sha256 "f457da3e4d134639f7c161b3a68f95a2a6b7d357d29cb8a1527de8c401ced7b9"
+      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.2.0/tailchat_linux_arm64.tar.gz"
+      sha256 "9876c610c512342a7ff34bc6227c37c2a5f13bf834e7013885c2a38d1730c1d2"
     end
   end
 
