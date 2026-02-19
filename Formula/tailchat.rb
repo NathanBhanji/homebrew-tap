6945cc01826ac0f77c5e8043d23e9e4807d998cd
NathanBhanji <nathan.bhanji@letsdothis.com>
2026-02-19 11:32:22 +0000

    Update tailchat to v0.1.1

--- Formula/tailchat.rb
+++ Formula/tailchat.rb
@@ -1,30 +1,30 @@
 class Tailchat < Formula
   desc "End-to-end encrypted terminal chat over Tailscale"
   homepage "https://github.com/NathanBhanji/tail-chat"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.0/tailchat_darwin_amd64.tar.gz"
-      sha256 "0320bbb6e674ea5e46ba057316a147d061fc59b8f92d077e4b84d91517bf5c68"
+      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.1/tailchat_darwin_amd64.tar.gz"
+      sha256 "ca690bf3c1036c1d9b9998e46db39bcf9fb303d08c7966488f0e3bc431bc3c44"
     end
 
     on_arm do
-      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.0/tailchat_darwin_arm64.tar.gz"
-      sha256 "0908460d8f324c12d3832a63f1fe7608b534352a1b293c5a32f30461ffa4ea8e"
+      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.1/tailchat_darwin_arm64.tar.gz"
+      sha256 "26b4e6741a3de37f38f36c67e9919e9d6e5cdc8c616523d82e3661f8b6cd7360"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.0/tailchat_linux_amd64.tar.gz"
-      sha256 "434d3e1367f5759a9999f0fcfeb06bc5acb1db5cd8634fc08df026b524ad9f11"
+      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.1/tailchat_linux_amd64.tar.gz"
+      sha256 "c9b975d47327b6dd7cd762d4dda8c70e745b6f7182f858e0e225373c5c932f09"
     end
 
     on_arm do
-      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.0/tailchat_linux_arm64.tar.gz"
-      sha256 "d37deda6152c1f4dc6c4b6f6be32693cf17d0bcff9dd4e65c91d22d378cf0295"
+      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.1/tailchat_linux_arm64.tar.gz"
+      sha256 "962ee94fa731cb6801f131cf4d4f35dd23fd6fa65136b5e6e87923d1ed083bab"
     end
   end
 
