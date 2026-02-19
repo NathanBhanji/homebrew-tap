cba24ce7d2af43e1a99878ee1d6c336edc950679
NathanBhanji <nathan.bhanji@letsdothis.com>
2026-02-19 12:17:31 +0000

    Update tailchat to v0.1.4

--- Formula/tailchat.rb
+++ Formula/tailchat.rb
@@ -1,30 +1,30 @@
 class Tailchat < Formula
   desc "End-to-end encrypted terminal chat over Tailscale"
   homepage "https://github.com/NathanBhanji/tail-chat"
-  version "0.1.3"
+  version "0.1.4"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.3/tailchat_darwin_amd64.tar.gz"
-      sha256 "da7046e25cb8162baedc4d0a23395344986fe36bb7394b70b37d24d95484864b"
+      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.4/tailchat_darwin_amd64.tar.gz"
+      sha256 "612c4af4daa0824df87259f0ea8d59304fd260e27131423eb52d0151eae084a0"
     end
 
     on_arm do
-      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.3/tailchat_darwin_arm64.tar.gz"
-      sha256 "517a755993c1b0f84c9ba1829729fa12e1bd802b429f23940322362d9aeb74b9"
+      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.4/tailchat_darwin_arm64.tar.gz"
+      sha256 "5df59f92fc27a5945ec42b7f45dec181a7f66103fcc34364eabaa04d91a32f4a"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.3/tailchat_linux_amd64.tar.gz"
-      sha256 "754a851134625d5b1afa41b2b0b0b5a06492f74ab6d100af5da137859a84fd0b"
+      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.4/tailchat_linux_amd64.tar.gz"
+      sha256 "bf44d3b0459a22b470bfcaabdb66d4e2533f008e1e11edc2337a4f7ccfa030ec"
     end
 
     on_arm do
-      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.3/tailchat_linux_arm64.tar.gz"
-      sha256 "f395a94397f71baf63adb79f254f41a0caf9670fcda3450bc19cab9c3095e708"
+      url "https://github.com/NathanBhanji/tail-chat/releases/download/v0.1.4/tailchat_linux_arm64.tar.gz"
+      sha256 "be67f8b3141b7580521ae6efdf030afd8e404d10feef70c85941b0d6a54c2865"
     end
   end
 
